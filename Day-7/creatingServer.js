d62d0124e7dcc2588b21e8fdd4ebeac5e947fcb7
ush_dxt <dixtushma@seasia.in>
2024-04-18 18:22:40 +0530

    creating http serverin nodejs

--- Day-7/creatingServer.js
+++ Day-7/creatingServer.js
@@ -1,9 +1,15 @@
+// import http module
 const http = require('http');
+
+// create server
 
 var server = http.createServer((req, res)=>{
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.end("Happy node js learning!");
 });
+
+// listen for server on specified port number
+
 server.listen(3000,'localhost',()=>{
     console.log("server is listening n port 3000");
 })
